17bc88f49a8572833b4781a0590a9bb8ec2becd3
Urvashi Singh <guman.urvashi@gmail.com>
2019-01-02 10:21:26 +1100

    payment confirmation

---
+++ app/src/main/java/com/p5m/me/data/PaymentConfirmationResponse.java
@@ -0,0 +1,117 @@
+package com.p5m.me.data;
+
+public class PaymentConfirmationResponse{
+
+
+        private String referenceId;
+        private String packageName;
+        private int amount;
+        private long date;
+        private String status;
+        private ClassDetailDtoBean classDetailDto;
+
+        public String getReferenceId() {
+            return referenceId;
+        }
+
+        public void setReferenceId(String referenceId) {
+            this.referenceId = referenceId;
+        }
+
+        public String getPackageName() {
+            return packageName;
+        }
+
+        public void setPackageName(String packageName) {
+            this.packageName = packageName;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public void setAmount(int amount) {
+            this.amount = amount;
+        }
+
+        public long getDate() {
+            return date;
+        }
+
+        public void setDate(long date) {
+            this.date = date;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public ClassDetailDtoBean getClassDetailDto() {
+            return classDetailDto;
+        }
+
+        public void setClassDetailDto(ClassDetailDtoBean classDetailDto) {
+            this.classDetailDto = classDetailDto;
+        }
+
+        public static class ClassDetailDtoBean {
+            /**
+             * classSessionId : 45740
+             * classDate : 2019-01-17
+             * fromTime : 13:00:00
+             * toTime : 14:00:00
+             * title : Super Yoga ++
+             */
+
+            private int classSessionId;
+            private String classDate;
+            private String fromTime;
+            private String toTime;
+            private String title;
+
+            public int getClassSessionId() {
+                return classSessionId;
+            }
+
+            public void setClassSessionId(int classSessionId) {
+                this.classSessionId = classSessionId;
+            }
+
+            public String getClassDate() {
+                return classDate;
+            }
+
+            public void setClassDate(String classDate) {
+                this.classDate = classDate;
+            }
+
+            public String getFromTime() {
+                return fromTime;
+            }
+
+            public void setFromTime(String fromTime) {
+                this.fromTime = fromTime;
+            }
+
+            public String getToTime() {
+                return toTime;
+            }
+
+            public void setToTime(String toTime) {
+                this.toTime = toTime;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+        }
+
+}
